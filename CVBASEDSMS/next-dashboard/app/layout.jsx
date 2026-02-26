bbbc787010b7e4b297668a7a2e55b94d47633c10
SUKIRTHAN UA <sukirthanudhayakumar.2024.cse@rajalakshmi.edu.in>
2026-02-26 20:07:12 +0530

    Final

--- CVBASEDSMS/next-dashboard/app/layout.jsx
+++ CVBASEDSMS/next-dashboard/app/layout.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3f144a7347e8bd4a8f975b48f06167784a140b9fd0cf518ff4a4a9e0a2c4361
-size 289
+oid sha256:a3fb5c2ec9562869ed512bb7582934319e19b9da159b916ed036d34552e36381
+size 288
--- CVBASEDSMS/next-dashboard/app/page.jsx
+++ CVBASEDSMS/next-dashboard/app/page.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7391ba20c6a2f22d705663de0fccde1faa1322ed0ecaeff7466807131cf478b1
-size 7151
+oid sha256:8c658b1f21846c74faf62da6906b4429121966c2e39c3acddbb77fef745f7f7d
+size 10149
